8615efd53ac037e0f045133cdaca44c321c25072
David Calhoun <dpcalhoun@gmail.com>
2017-11-28 07:21:13 -0600

    Add emotion

--- components/Index.js
+++ components/Index.js
@@ -1,3 +1,30 @@
 import React from 'react';
+import styled, { css, injectGlobal } from 'react-emotion';
 
-export default props => <h1>Hello, World!</h1>;
+injectGlobal`
+  body {
+    font-family: sans-serif;
+    margin: 0;
+  }
+`
+
+const Container = styled('div')`
+  background: #ccc;
+  padding: 1rem;
+`
+
+const myStyle = css`
+  color: white;
+  margin: 0;
+`
+
+const Body = styled('p')({
+  color: 'red',
+})
+
+export default props => (
+  <Container>
+    <h1 className={myStyle}>Hello, World!</h1>
+    <Body>I am Body.</Body>
+  </Container>
+)

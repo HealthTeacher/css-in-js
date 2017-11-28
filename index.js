9b1abd2e1fa0164b6ba5fe167323da35edfae409
David Calhoun <dpcalhoun@gmail.com>
2017-11-28 06:50:18 -0600

    Setup HMR

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components/Index';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<Index />, document.getElementById('js-root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('js-root')
+  );
+};
+
+render(Index);
+
+if (module.hot) {
+  module.hot.accept('./components/Index', () => {
+    render(Index);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'index.js'),
+    app: ['react-hot-loader/patch', path.resolve(__dirname, 'index.js')],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     sourceMapFilename: '[name].map',
+    publicPath: '/',
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
@@ -17,5 +19,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
   ],
+  devServer: {
+    hot: true,
+  },
 };

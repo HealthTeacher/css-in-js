12835cd18147406e5f4fd395d7813d44741d56cc
David Calhoun <dpcalhoun@gmail.com>
2017-11-28 09:53:48 -0600

    feat(lib): Add Styletron
    Add [Styletron](http://styletron.js.org) example.

--- components/Index.js
+++ components/Index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import Emotion from './Emotion';
+import Styletron from './Styletron';
+import StyletronClient from 'styletron-client';
+import {StyletronProvider} from 'styletron-react';
 
-const Index = props => <Emotion />;
+const Index = props => (
+  <div>
+    <StyletronProvider styletron={new StyletronClient()}>
+      <div>
+        <Emotion />
+        <Styletron />
+      </div>
+    </StyletronProvider>,
+  </div>
+);
 
 export default Index;
